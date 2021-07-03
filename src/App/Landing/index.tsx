f99c73034a6fa4cdd4408833f10ea40404221e9f
Mihail Marian <mihailmarian12@gmail.com>
2021-07-03 19:08:07 +0100

    landing page content

--- src/App/Landing/index.tsx
+++ src/App/Landing/index.tsx
@@ -1,14 +1,18 @@
 import * as React from "react";
+import demoImage from "./product_demo.jpg";
 import "./index.scss";
 
 const Landing: React.FC = () => (
   <section className="landing">
-    <h1>
-      Tired of doomscrolling just to read posts from your favourite LinkedIn
-      personalities? Enter <span className="italic">rellate</span>.
-    </h1>
-    <a href="#form">Get started</a>
     <div>
+      <h1>
+        Tired of doomscrolling just to read posts from your favourite LinkedIn
+        personalities? Enter <span className="italic">rellate</span>.
+      </h1>
+      <a href="#form">Get started</a>
+    </div>
+
+    <div className="pitch">
       <p>
         You managed to find people that post amazing content on LinkedIn. But
         now you have to spend a ton of time scrolling through your feed in order
@@ -20,6 +24,7 @@ const Landing: React.FC = () => (
         you all the posts from your chosen LinkedIn influencers as a newsletter.
       </p>
     </div>
+    <img src={demoImage} alt="product demo" />
   </section>
 );
 
