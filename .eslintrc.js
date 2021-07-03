980cfbdfa140c7e95a60452934935c78f3c4304e
Mihail Marian <mihailmarian12@gmail.com>
2021-07-03 14:07:06 +0100

    eslintrc update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,23 @@
 module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: [
     '@typescript-eslint',
+    'react'
   ],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
   ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
 };
