c5ffa192b4a286898deef20bb2eb88b14f3b5d71
Mihail Marian <mihailmarian12@gmail.com>
2021-07-03 16:01:38 +0100

    first content to header

--- src/App/Header/index.tsx
+++ src/App/Header/index.tsx
@@ -1,5 +1,14 @@
 import * as React from "react";
+import "./index.scss";
 
-const Header: React.FC = () => <header />;
+const Header: React.FC = () => (
+  <header>
+    <div>rellate</div>
+    <ul>
+      <li>Get Started</li>
+      <li>Contact</li>
+    </ul>
+  </header>
+);
 
 export default Header;
