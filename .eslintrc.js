c37ed0d3e3833cc1805e50a9cbe6c6b40f358105
Mihail Marian <mihailmarian12@gmail.com>
2021-07-03 14:19:07 +0100

    prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,21 +4,18 @@ module.exports = {
     es2021: true,
   },
   root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-    'react'
-  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "react"],
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier'
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
   ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 12,
-    sourceType: 'module',
+    sourceType: "module",
   },
 };
