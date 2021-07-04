d11598e1c4b23f0ba1253e24e50b6f66eef0fa96
Mihail Marian <mihailmarian12@gmail.com>
2021-07-04 09:56:31 +0100

    skeleton for form section

---
+++ src/App/Form/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import "./index.scss";
+
+const Form: React.FC = () => (
+  <section id="form" className="form">
+    <div>hello</div>
+  </section>
+);
+
+export default Form;
--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import Landing from "./Landing";
+import Form from "./Form";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Landing />
+      <Form />
     </div>
   );
 }
