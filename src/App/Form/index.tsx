9c5864c86dd5e0f21422bb4fd7a62f95c6316998
Mihail Marian <mihailmarian12@gmail.com>
2021-07-05 10:08:00 +0100

    added email state

--- src/App/Form/index.tsx
+++ src/App/Form/index.tsx
@@ -1,20 +1,30 @@
 import * as React from "react";
+import { FC, useState } from "react";
 import { FormControl, InputLabel, Input } from "@material-ui/core";
 
 import "./index.scss";
 
-const Form: React.FC = () => (
-  <section id="form" className="form">
-    <h2>Set up your newsletter in less than a minute</h2>
-    <form>
-      <FormControl className="form-control">
-        <InputLabel shrink className="input-label">
-          1/3 - Enter your email
-        </InputLabel>
-        <Input required placeholder="name@example.com" />
-      </FormControl>
-    </form>
-  </section>
-);
+const Form: FC = () => {
+  const [email, setEmail] = useState("");
+
+  return (
+    <section id="form" className="form">
+      <h2>Set up your newsletter in less than a minute</h2>
+      <span>{email}</span>
+      <form>
+        <FormControl className="form-control">
+          <InputLabel shrink className="input-label">
+            1/3 - Enter your email
+          </InputLabel>
+          <Input
+            onChange={(event) => setEmail(event.target.value)}
+            required
+            placeholder="name@example.com"
+          />
+        </FormControl>
+      </form>
+    </section>
+  );
+};
 
 export default Form;
