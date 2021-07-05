40e4c4e5e13a0a0d45f0c7439b86ab5904917a83
Mihail Marian <mihailmarian12@gmail.com>
2021-07-05 07:16:00 +0100

    started work on form section

--- src/App/Form/index.tsx
+++ src/App/Form/index.tsx
@@ -3,7 +3,10 @@ import "./index.scss";
 
 const Form: React.FC = () => (
   <section id="form" className="form">
-    <div>hello</div>
+    <h2>Set up your newsletter in less than a minute</h2>
+    <form>
+      <div />
+    </form>
   </section>
 );
 
