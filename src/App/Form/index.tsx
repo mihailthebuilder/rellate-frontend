52a3dde36c6b64728f2f77cb65a5f71a1ff34058
Mihail Marian <mihailmarian12@gmail.com>
2021-07-05 07:36:48 +0100

    material ui email input

--- src/App/Form/index.tsx
+++ src/App/Form/index.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
+import { TextField, InputLabel } from "@material-ui/core";
+
 import "./index.scss";
 
 const Form: React.FC = () => (
   <section id="form" className="form">
     <h2>Set up your newsletter in less than a minute</h2>
     <form>
-      <div />
+      <InputLabel htmlFor="emailInput">1/3 - Enter your email</InputLabel>
+      <TextField id="emailInput" required placeholder="name@example.com" />
     </form>
   </section>
 );
