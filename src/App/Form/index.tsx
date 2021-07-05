e178e855ca88319f034b6c9cf3878e36784aefa5
Mihail Marian <mihailmarian12@gmail.com>
2021-07-05 09:05:18 +0100

    saving work so far with material ui

--- src/App/Form/index.tsx
+++ src/App/Form/index.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { TextField, InputLabel } from "@material-ui/core";
+import { FormControl, InputLabel, Input } from "@material-ui/core";
 
 import "./index.scss";
 
@@ -7,8 +7,12 @@ const Form: React.FC = () => (
   <section id="form" className="form">
     <h2>Set up your newsletter in less than a minute</h2>
     <form>
-      <InputLabel htmlFor="emailInput">1/3 - Enter your email</InputLabel>
-      <TextField id="emailInput" required placeholder="name@example.com" />
+      <FormControl className="form-control">
+        <InputLabel shrink className="input-label">
+          1/3 - Enter your email
+        </InputLabel>
+        <Input placeholder="name@example.com" />
+      </FormControl>
     </form>
   </section>
 );
