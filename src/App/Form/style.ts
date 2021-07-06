7b76f39995778639a863501a7f75f2524831f7cd
Mihail Marian <mihailmarian12@gmail.com>
2021-07-06 13:55:51 +0100

    moved input to styled

--- src/App/Form/style.ts
+++ src/App/Form/style.ts
@@ -1,6 +1,20 @@
 import styled from "styled-components";
 import { Input } from "@material-ui/core";
+import { yellow } from "../../resources/style";
 
-const IStyled = styled(Input)``;
+const { lime } = yellow;
+
+const IStyled = styled(Input)`
+  font-size: 1.3em;
+  color: ${lime};
+
+  &::before {
+    border-bottom-color: white !important;
+  }
+
+  &::after {
+    border-bottom-color: ${lime};
+  }
+`;
 
 export default IStyled;
